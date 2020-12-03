d1b99e4443829808d2fbd9dd1971f7d283ed53ab
Mkona055 <mkona055@uottawa.ca>
2020-12-03 16:04:42 -0500

    SelectServices for Customer is ready

---
+++ app/src/main/java/ca/novigrad/FillForm.java
@@ -0,0 +1,14 @@
+package ca.novigrad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FillForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_form);
+    }
+}
