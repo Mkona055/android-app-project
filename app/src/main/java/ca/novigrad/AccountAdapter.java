9a443ec4c407e1fc7935d4338a8641c401bf1137
lmerc037 <62909161+lmerc037@users.noreply.github.com>
2020-12-05 23:13:34 -0500

    search stuff

--- app/src/main/java/ca/novigrad/AccountAdapter.java
+++ app/src/main/java/ca/novigrad/AccountAdapter.java
@@ -3,7 +3,6 @@ package ca.novigrad;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -49,10 +48,10 @@ public class AccountAdapter extends FirestoreRecyclerAdapter<AccountList, Accoun
         public AccountHolder(@NonNull View itemView) {
             super(itemView);
 
-            email = itemView.findViewById(R.id.textViewEmail3);
-            fullName = itemView.findViewById(R.id.textViewFullName3);
-            phone = itemView.findViewById(R.id.textViewPhoneNumber3);
-            role = itemView.findViewById(R.id.textViewRole3);
+            email = itemView.findViewById(R.id.textViewBranchID4);
+            fullName = itemView.findViewById(R.id.textViewBranchAddress4);
+            phone = itemView.findViewById(R.id.textViewPhoneNumber4);
+            role = itemView.findViewById(R.id.textViewEmail4);
 
 
         }
--- app/src/main/java/ca/novigrad/ManageAccount.java
+++ app/src/main/java/ca/novigrad/ManageAccount.java
@@ -6,10 +6,7 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.CollectionReference;
